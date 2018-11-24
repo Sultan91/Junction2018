a1059624b02e1a80191819ff8a4fa4e22f5cd06f
Andrew <a.ermak@innopolis.ru>
2018-11-24 14:19:50 +0200

    Dicts -> Tuples, added pictures generation

--- queryes.py
+++ queryes.py
@@ -1,6 +1,6 @@
 import json
 from datetime import datetime
-from typing import Dict, Optional, List, Callable
+from typing import Dict, Optional, List, Callable, Tuple
 
 import requests
 
@@ -17,29 +17,29 @@ def get_thermalimage_by_dates(date_from: datetime, date_to: datetime, item_count
     return __post_query_to_site(data=data, item_count=item_count, start_index=start_index, path='thermalimage')
 
 
-def extract_data_from_thermalimage_response(response: requests.Response) -> List[Dict]:
+def extract_data_from_thermalimage_response(response: requests.Response) -> List[Tuple]:
     """
     Extracting image information from response
     :param response: response from thermalimage
-    :return: list of dicts in format [{'date': 'image_array'}, {...}, ... ]
+    :return: list of tuples in format [('date', 'image_array'), (...), ... ]
     """
     if response.ok:
         items = response.json()['data']['items']
         result_list = []
         for item in items:
             item_time = item['captureTime']
-            result_list.append({item_time: item['image']})
+            result_list.append((item_time, item['image']))
         return result_list
     return []
 
 
-def get_all_data_from_device(func: Callable, func_kwargs: Dict, func_extract: Callable) -> List[Dict]:
+def get_all_data_from_device(func: Callable, func_kwargs: Dict, func_extract: Callable) -> List[Tuple]:
     """
     Returned list of data using 'function' and 'function_filter'
     :param func: function to get data
     :param func_kwargs: kwargs for the above function
     :param func_extract: function to extract_data
-    :return: List of Dicts, which contains data
+    :return: List of Tuples, which contains data in format: [('date', 'image_array'), (...), ... ]
     """
     _item_count = 5
     _start_index = 0
@@ -51,15 +51,16 @@ def get_all_data_from_device(func: Callable, func_kwargs: Dict, func_extract: Ca
     while is_data:
         # get and process data
         response: requests.Response = func(**func_kwargs)
-        returned_list = func_extract(response)
-        result_list.extend(returned_list)
+        if response.ok:
+            returned_list = func_extract(response)
+            result_list.extend(returned_list)
 
-        # check if there is more data
-        is_data = response.json()['data']['hasMoreItems']
+            # check if there is more data
+            is_data = response.json()['data']['hasMoreItems']
 
-        # change start index position to new data
-        _start_index = _start_index + response.json()['data']['currentItemCount']
-        func_kwargs['start_index'] = _start_index
+            # change start index position to new data
+            _start_index = _start_index + response.json()['data']['currentItemCount']
+            func_kwargs['start_index'] = _start_index
 
     return result_list
 
@@ -91,8 +92,10 @@ def __post_query_to_site(data: Dict, item_count: int = 0, start_index: int = 0, 
 
 if __name__ == '__main__':
     # An example for thermalimage
-    date_from = datetime(year=2018, month=10, day=10, hour=11, minute=25)
+    date_from = datetime(year=2018, month=10, day=10, hour=11, minute=55)
     date_to = datetime(year=2018, month=10, day=10, hour=12, minute=0)
     data = get_all_data_from_device(get_thermalimage_by_dates, {'date_from': date_from, 'date_to': date_to},
                                     extract_data_from_thermalimage_response)
-    print(data)
+
+    for timestamp, d in data:
+        print(timestamp, d, '\n')
---
+++ thermal_data_visualization.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from typing import List
+
+from PIL import Image
+
+from queryes import get_all_data_from_device, get_thermalimage_by_dates, extract_data_from_thermalimage_response
+
+
+def from_data_to_image(data: List[List], image_name: str) -> None:
+    """
+    Saves image to 'images/' dir
+    """
+    img = Image.new('RGB', (24, 32), color='black')
+    minimum = 100
+    maximum = 0
+    for row in data:
+        for cell in row:
+            minimum = min(minimum, cell)
+            maximum = max(maximum, cell)
+
+    for i, row in enumerate(data):
+        for j, cell in enumerate(row):
+            img.putpixel((i, j), ((int(255 * (maximum - cell) / (maximum - minimum))), 0, 0))
+
+            img.save(f'images/{image_name}.png')
+
+
+if __name__ == "__main__":
+    date_from = datetime(year=2018, month=10, day=10, hour=11, minute=55)
+    date_to = datetime(year=2018, month=10, day=10, hour=12, minute=0)
+    all_data = get_all_data_from_device(get_thermalimage_by_dates, {'date_from': date_from, 'date_to': date_to},
+                                        extract_data_from_thermalimage_response)
+    for image_name, a_picture_data in all_data:
+        from_data_to_image(a_picture_data, image_name)
